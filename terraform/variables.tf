915f7fdc6a67a6cd21d9ff5fa85a6ee56ec67eb8
Breta Osod <bretaosodo@gmail.com>
2026-09-03 23:00:46 +0300

    updated file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,21 @@
 variable "admin_cidr" {
   description = "CIDR vlock allowed to reach SSH and the pipeline's admin UIs (GRAFANA, KAFDROP, SPARK UI, pgAdmin"
-  type = string
+  type        = string
 }
 
 variable "key_name" {
   description = "Name of an existing EC@ key pair to allow SSH access"
-  type = string
+  type        = string
 }
 
 variable "instance_type" {
   description = "EC2 instance type for the pipeline host"
-  type = string
-  default = "t3.large"
+  type        = string
+  default     = "t3.large"
 }
 
 variable "root_volume_size_gb" {
   description = "Root EBS volume size in GN. The full docker- compose stack needs headroom for images and checkpoints"
-  type = number
-  default = 60
+  type        = number
+  default     = 60
 }
